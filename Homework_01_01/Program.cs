01fb81fd96cc40488871ffa52db2e6fcd0524e0d
John <jeden667@gmail.com>
2022-10-14 18:40:17 +0400

    General fixes in Homework

--- Homework_01_01/Program.cs
+++ Homework_01_01/Program.cs
@@ -1,21 +1,26 @@
-﻿Console.Clear();
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Clear();
 Console.WriteLine("<< Большее и меньшее число >>");
-Console.WriteLine(" ");
-Console.WriteLine("Введите первое число: ");
-string name1 = Console.ReadLine();
-int num1 = Convert.ToInt32(name1);
-Console.WriteLine("Введите второе число: ");
-string name2 = Console.ReadLine();
-int num2 = Convert.ToInt32(name2);
-Console.WriteLine(" ");
+Console.WriteLine();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 if (num1 > num2)
 {
-    Console.WriteLine(">> Первое число " + num1 + " - БОЛЬШЕ");
-    Console.WriteLine(">> Второе число " + num2 + " - МЕНЬШЕ");
+    Console.WriteLine($">> Первое число {num1} - БОЛЬШЕ");
+    Console.WriteLine($">> Второе число {num2} - МЕНЬШЕ");
 }
 else
 {
-    Console.WriteLine(">> Второе число " + num2 + " - БОЛЬШЕ");
-    Console.WriteLine(">> Первое число " + num1 + " - МЕНЬШЕ");
+    Console.WriteLine($">> Второе число {num2} - БОЛЬШЕ");
+    Console.WriteLine($">> Первое число {num1} - МЕНЬШЕ");
 }
-Console.WriteLine(" ");
+Console.WriteLine();
--- Homework_01_02/Program.cs
+++ Homework_01_02/Program.cs
@@ -1,21 +1,24 @@
-﻿Console.Clear();
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Clear();
 Console.WriteLine("<< Поиск максимального числа >>");
-Console.WriteLine(" ");
-Console.WriteLine("Введите первое число: ");
-string name1 = Console.ReadLine();
-int num1 = Convert.ToInt32(name1);
-Console.WriteLine("Введите второе число: ");
-string name2 = Console.ReadLine();
-int num2 = Convert.ToInt32(name2);
-Console.WriteLine("Введите третье число: ");
-string name3 = Console.ReadLine();
-int num3 = Convert.ToInt32(name3);
+Console.WriteLine();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
 
-Console.WriteLine(" ");
-Console.Write(">> Максимальное число: ");
-Console.WriteLine(max);
-Console.WriteLine(" ");
+Console.WriteLine();
+Console.Write($">> Максимальное число: {max}");
+Console.WriteLine();
+Console.WriteLine();
